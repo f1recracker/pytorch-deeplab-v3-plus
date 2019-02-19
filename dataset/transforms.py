4444eedc26ea93c5f31461b5941b2836d7150441
f1recracker <harshweb@gmail.com>
2019-02-19 13:11:26 -0800

    Increased scale transform scale, added rotations

--- dataset/transforms.py
+++ dataset/transforms.py
@@ -8,17 +8,28 @@ import torchvision.transforms.functional as tfunc
 from PIL import Image
 
 def transforms(img, seg, size=(360, 640), hflip=True, five_crop=True,
-               tensor=True, normalize=True):
+               rotate=True, tensor=True, normalize=True):
     ''' BDD transforms pipeline '''
-    
+
     if hflip and random.random() < 0.5:
         img = tfunc.hflip(img)
         seg = tfunc.hflip(seg)
 
     if five_crop and random.random() < 0.5:
         i = random.randint(0, 4)
-        img = tfunc.five_crop(img, (size[0] // 2, size[1] // 2))[i]
-        seg = tfunc.five_crop(seg, (size[0] // 2, size[1] // 2))[i]
+        img = tfunc.five_crop(img, (size[0] * 0.8, size[1] * 0.8))[i]
+        seg = tfunc.five_crop(seg, (size[0] * 0.8, size[1] * 0.8))[i]
+
+    if rotate and random.random() < 0.5:
+        angle = random.randrange(-30, 30)
+        mask = Image.new('1', seg.size, (1,))
+
+        img = tfunc.rotate(img, angle)
+        seg = tfunc.rotate(seg, angle)
+        mask = tfunc.rotate(mask, angle)
+
+        white = Image.new('L', seg.size, (0xFF))
+        seg = Image.composite(seg, white, mask=mask)
 
     img = tfunc.resize(img, size, interpolation=Image.NEAREST)
     seg = tfunc.resize(seg, size, interpolation=Image.NEAREST)
