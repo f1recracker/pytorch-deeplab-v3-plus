5bfb357717c34239d02830653b11165f81a4ccdf
f1recracker <harshweb@gmail.com>
2019-02-23 18:38:05 -0800

    Updated color palette using BDD colors

--- dataset/bdd.py
+++ dataset/bdd.py
@@ -29,32 +29,32 @@ class BDDSegmentationDataset(Dataset):
         return len(self.images)
 
     @staticmethod
-    def palette(seg):
+    def palette(seg_labels):
         color_map = {
-            0: torch.FloatTensor((0xab, 0x47, 0xbc)) / 0xFF, # Road
-            1: torch.FloatTensor((0x6a, 0x1b, 0x9a)) / 0xFF, # Sidewalk
-            2: torch.FloatTensor((0x60, 0x7d, 0x8b)) / 0xFF, # Building
-            3: torch.FloatTensor((0x8d, 0x6e, 0x63)) / 0xFF, # Wall
-            4: torch.FloatTensor((0xcf, 0xd8, 0xdc)) / 0xFF, # Fence
-            5: torch.FloatTensor((0xf5, 0xf5, 0xf5)) / 0xFF, # Pole
-            6: torch.FloatTensor((0xff, 0xc4, 0x00)) / 0xFF, # Light
-            7: torch.FloatTensor((0xff, 0xff, 0x00)) / 0xFF, # Sign
-            8: torch.FloatTensor((0x00, 0xc8, 0x53)) / 0xFF, # Vegetation
-            9: torch.FloatTensor((0xb2, 0xff, 0x59)) / 0xFF, # Terrain
-            10: torch.FloatTensor((0xb2, 0xdf, 0xdb)) / 0xFF, # Sky
-            11: torch.FloatTensor((0xff, 0x17, 0x44)) / 0xFF, # Person
-            12: torch.FloatTensor((0xff, 0x8a, 0x80)) / 0xFF, # Rider
-            13: torch.FloatTensor((0x3f, 0x51, 0xb5)) / 0xFF, # Car
-            14: torch.FloatTensor((0x1a, 0x23, 0x7e)) / 0xFF, # Truck
-            15: torch.FloatTensor((0xe8, 0xea, 0xf6)) / 0xFF, # Bus
-            16: torch.FloatTensor((0x29, 0xb6, 0xf6)) / 0xFF, # Train
-            17: torch.FloatTensor((0x00, 0x69, 0x5c)) / 0xFF, # Motorcycle
-            18: torch.FloatTensor((0x4d, 0xb6, 0xac)) / 0xFF, # Bicycle
+            0: torch.FloatTensor((128, 67, 125)), # Road
+            1: torch.FloatTensor((247, 48, 227)), # Sidewalk
+            2: torch.FloatTensor((72, 72, 72)), # Building
+            3: torch.FloatTensor((101, 103, 153)), # Wall
+            4: torch.FloatTensor((190, 151, 152)), # Fence
+            5: torch.FloatTensor((152, 152, 152)), # Pole
+            6: torch.FloatTensor((254, 167, 56)), # Light
+            7: torch.FloatTensor((221, 217, 55)), # Sign
+            8: torch.FloatTensor((106, 140, 51)), # Vegetation
+            9: torch.FloatTensor((146, 250, 157)), # Terrain
+            10: torch.FloatTensor((65, 130, 176)), # Sky
+            11: torch.FloatTensor((224, 20, 64)), # Person
+            12: torch.FloatTensor((255, 0, 25)), # Rider
+            13: torch.FloatTensor((0, 22, 138)), # Car
+            14: torch.FloatTensor((0, 11, 70)), # Truck
+            15: torch.FloatTensor((0, 63, 98)), # Bus
+            16: torch.FloatTensor((0, 82, 99)), # Train
+            17: torch.FloatTensor((0, 36, 224)), # Motorcycle
+            18: torch.FloatTensor((121, 17, 38)), # Bicycle
         }
-        seg_color = torch.zeros_like(seg).repeat(3, 1, 1).float()
+        seg_colors = torch.zeros_like(seg_labels).repeat(3, 1, 1).float()
         for c_id, color in color_map.items():
-            seg_color += (seg == c_id).float()[None, :, :] * color[:, None, None]
-        return seg_color
+            seg_colors += ((seg_labels == c_id).float()[None, :, :] * color[:, None, None]) / 255.0
+        return seg_colors
 
     def __getitem__(self, key):
         image = Image.open(self.images[key])
